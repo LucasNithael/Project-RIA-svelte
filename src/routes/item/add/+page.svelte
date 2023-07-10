fee37f3af988af3e2212031eee9536e1f3d81c8c
nithael <lucasnithael0101@gmail.com>
2023-07-10 00:40:18 -0300

    feat: register item

---
+++ src/routes/item/add/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { postApi } from "../../../api/api";
+    
+    let item = {
+        name: '',
+        description: ''
+    }
+
+    const addItem = async () => {
+        try{
+            await postApi('/itens/', item)
+            console.log('Item cadastrado')
+        } catch(error){
+            console.log(error)
+        }
+    }
+</script>
+
+
+
+<form on:submit={() => addItem()}>
+    <label for="name">Name</label>
+    <input type="text" id="name" bind:value={item.name}>
+    <label for="description">Description</label>
+    <input type="text" id="description" bind:value={item.description}>
+    <button>Save</button>
+</form>

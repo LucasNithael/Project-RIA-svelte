65503b4c0b778dc9d06a2092bba45703eda52563
nithael <lucasnithael0101@gmail.com>
2023-07-10 01:19:20 -0300

    feat: register client

---
+++ src/routes/client/add/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { postApi } from "../../../api/api";
+    
+    let client = {
+        name: '',
+        email: ''
+    }
+
+    const addClient = async () => {
+        try{
+            await postApi('/clients/', client)
+            console.log('client cadastrado')
+        } catch(error){
+            console.log(error)
+        }
+    }
+</script>
+
+
+
+<form on:submit={() => addClient()}>
+    <label for="name">Name</label>
+    <input type="text" id="name" bind:value={client.name}>
+    <label for="email">Email</label>
+    <input type="text" id="email" bind:value={client.email}>
+    <button>Save</button>
+</form>

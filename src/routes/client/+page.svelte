5d04d3c945c94444d3560e60f02561d098ff12ef
nithael <lucasnithael0101@gmail.com>
2023-07-10 01:13:22 -0300

    feat: add client route

---
+++ src/routes/client/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { onMount } from 'svelte';
+    import { getApi, deleteApi} from '../../api/api'
+ 
+    
+    /**
+     * @type {any[]}
+     */
+    let clients = []
+
+    onMount(async () => {
+        try{
+            clients = await getApi('/clients/')
+            console.log(clients)
+        } catch (error){
+            console.log(error)
+        }
+    })
+
+
+    // @ts-ignore
+    const deleteClient = async (id) => {
+        try{
+            await deleteApi(`/clients/${id}/`)
+            console.log(`cliente deletado de id ${id}`)
+        } catch (error){
+            console.log(error)
+        }
+    }
+    
+</script>
+
+
+
+{#each clients as {id, name, email}}
+    <h2>{name}</h2>
+    <p>{email}</p>
+    <button on:click={() => deleteClient(id)}>delete</button>
+    <hr>
+{/each}
